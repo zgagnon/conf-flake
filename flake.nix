3dbdac030917efebbeee4c9029e6c3b7544e1cb5
GitButler <gitbutler@gitbutler.com>
2024-03-11 16:53:44 -0400

    GitButler Integration Commit
    This is an integration commit for the virtual branches that GitButler is tracking.
    
    Due to GitButler managing multiple virtual branches, you cannot switch back and
    forth between git branches and virtual branches easily. 
    
    If you switch to another branch, GitButler will need to be reinitialized.
    If you commit on this branch, GitButler will throw it away.
    
    Here are the branches that are currently applied:
     - master (refs/gitbutler/master)
       branch head: 129675a9dc206d8ed55fc9c18d267b9311788d08
       - hosts/Zells-MacBook-Pro/default.nix
       - hosts/Zells-MacBook-Pro/default.org
       - flake.nix
       - flake.org
       - switch.org
       - switch
       - hosts/Zells-MacBook-Pro/.gitmessage
    
    Your previous branch was: refs/heads/master
    
    The sha for that commit was: c2ec93d089d019a796c504f541ad3ab7f1dc7023
    
    For more information about what we're doing here, check out our docs:
    https://docs.gitbutler.com/features/virtual-branches/integration-branch

--- flake.nix
+++ flake.nix
@@ -1,21 +1,38 @@
+# Flake.nix
+# :PROPERTIES:
+# :header-args: :tangle flake.nix :comments org
+# :END:
+
+# The flake.nix is the over-arching control point for the configuration. It is responsible for gathering the various package sources and applying the correct configuration for each machine.
+
+
 {
-  description = "Mac Machine Configuration Flake";
+  description = "Machine Configuration Flake";
 
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    home-manager.url = "github:nix-community/home-manager/release-23.11";
-    darwin.url = "github:lnl7/nix-darwin";
-    darwin.inputs.nixpkgs.follows = "nixpkgs";
-  };
 
-  outputs = { self, nixpkgs, home-manager, darwin }: {
-   
-   darwinConfigurations."Zells-MacBook-Pro" = darwin.lib.darwinSystem {
-    system = "aarch64-darwin";
-    modules = [
-      home-manager.darwinModules.home-manager 
-      ./hosts/Zells-MacBook-Pro/default.nix
-     ];
-   };
-  };
+
+# These inputs provide the channels the flake will consume
+
+inputs = {
+  nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  home-manager.url = "github:nix-community/home-manager/release-23.11";
+  darwin.url = "github:lnl7/nix-darwin";
+  darwin.inputs.nixpkgs.follows = "nixpkgs";
+};
+
+outputs = { self, nixpkgs, home-manager, darwin, ... }: {
+
+
+
+# Configuration for the Mechanical Orchard work Macbook
+
+darwinConfigurations."Zells-MacBook-Pro" = darwin.lib.darwinSystem {
+  system = "aarch64-darwin";
+  modules = [
+    home-manager.darwinModules.home-manager
+    ./hosts/Zells-MacBook-Pro/default.nix
+   ];
+ };
+};
+
 }
