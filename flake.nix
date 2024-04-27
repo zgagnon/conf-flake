4378577980cf1a07418770be32f35f0fdae75199
zgagnon <zell@mechanical-orchard.com>
2024-04-26 20:40:10 -0400

    Explain main README a bit better
    Also don't do the small chunks in code. Say what you will do,
    then do it

--- flake.nix
+++ flake.nix
@@ -1,22 +1,18 @@
 {
   description = "Machine Configuration Flake";
-
-inputs = {
-  nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-  home-manager.url = "github:nix-community/home-manager/release-23.11";
-  darwin.url = "github:lnl7/nix-darwin";
-  darwin.inputs.nixpkgs.follows = "nixpkgs";
-};
-
-outputs = { self, nixpkgs, home-manager, darwin, ... }: {
-
-darwinConfigurations."Zells-MacBook-Pro" = darwin.lib.darwinSystem {
-  system = "aarch64-darwin";
-  modules = [
-    home-manager.darwinModules.home-manager
-    ./hosts/Zells-MacBook-Pro/default.nix
-   ];
- };
-};
-
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    home-manager.url = "github:nix-community/home-manager/release-23.11";
+    darwin.url = "github:lnl7/nix-darwin";
+    darwin.inputs.nixpkgs.follows = "nixpkgs";
+  };
+  outputs = { self, nixpkgs, home-manager, darwin, ... }: {
+   darwinConfigurations."Zells-MacBook-Pro" = darwin.lib.darwinSystem {
+    system = "aarch64-darwin";
+    modules = [
+      home-manager.darwinModules.home-manager
+      ./hosts/Zells-MacBook-Pro/default.nix
+     ];
+   };
+  };
 }
