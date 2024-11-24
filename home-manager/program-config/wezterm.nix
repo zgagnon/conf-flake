e069fb22eab9f12a934e28381aa2f803f709df92
zgagnon <zoe.gagnon@mechanical-orchard.com>
2024-11-24 00:41:20 -0500

    Sun Nov 24 00:41:20 EST 2024

--- home-manager/program-config/wezterm.nix
+++ home-manager/program-config/wezterm.nix
@@ -9,7 +9,7 @@
     config.font = wezterm.font("JetBrains Mono")
     config.font_size = 18
     config.color_scheme = "Tokyo Night Moon"
-    config.window_background_opacity = 0.75
+    config.window_background_opacity = 0.90
     config.hide_tab_bar_if_only_one_tab = true
     
     return config
